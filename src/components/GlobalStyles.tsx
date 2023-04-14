89f4ccba95af06e2408c09bda6d61205c08d7857
MaximeJeannette <102291390+MaximeJeannette@users.noreply.github.com>
2023-04-14 14:11:29 +0200

    misc: setup font and reset css

--- src/components/GlobalStyles.tsx
+++ src/components/GlobalStyles.tsx
@@ -1,9 +1,11 @@
 import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 
 const GlobalStyles = createGlobalStyle`
+  ${reset}
   html,
   body {
- 
+    font-family: 'SF Pro Display', sans-serif;
   }
 `;
 
